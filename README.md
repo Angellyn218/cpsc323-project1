a4a9192c5539f8598609a4592d7132997cbc950e
Christopher Liwanag <cliwanag@csu.fullerton.edu>
2023-11-10 01:35:17 -0800

    final finishes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# cpsc323-project1
+# CPSC 323 - Compilers & Languages
+
+## Programming Assignment 1
+### Group Memebers:
+* Angel Cervantes
+* Christopher Liwanag
+* Shuo Wang
+### Problem Statement:
+Write a program to build a lexical analyzer. The lexical analyzer should accept a .txt file as an input, consisting of code snippets and comments, and it gives output by tokenizing the input file.
+### Due:
+November 10, 2023
+### Usage:
+In the "project1.cpp" file, you can edit the "INPUT_FILE_PATH" and "INPUT_FILE_NAME" as needed. The output will go the same provided directory as the input and be named "output.txt".
--- project1.cpp
+++ project1.cpp
@@ -1,15 +1,28 @@
-//
-//
-//
-//
+/*  CPSC 323 - Compilers & Languages
+    Programming Assignment 1
+    Group Memebers:
+        Angel Cervantes
+        Christopher Liwanag
+        Shuo Wang
+    Problem Statement:
+        Write a program to build a lexical analyzer. The lexical analyzer should accept a .txt file as an
+        input, consisting of code snippets and comments, and it gives output by tokenizing the input file.
+    Due:
+        November 10, 2023
+*/
 
 #include "lex_analyzer.h"
+
+// identify your input file directory here
+#define INPUT_FILE_PATH "/Users/quis/Documents/cpsc323_f23_src/"
+// identify your input file name including extension here
+#define INPUT_FILE_NAME "input.txt"
 
 int main()
 {
     LexAnalyzer lexA{};
 
-    lexA.analyze("/Users/quis/Documents/cpsc323_f23_src/", "input.txt");
+    lexA.analyze(INPUT_FILE_PATH, INPUT_FILE_PATH);
 
     return 0;
 }
